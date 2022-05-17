6d742a47186d283ed7b8d2fff7989b846ad3382f
SHANAZAR NIZAMADDINOV <snizamaddinow@gmail.com>
2022-05-18 00:30:20 +0300

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;  //BitmapImage namespace
+
+namespace BookStore
+{
+    internal class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }  
+        public double price { get; set; }
+        public BitmapImage image { get; set; }
+
+        public Book(string id, string t, double p, BitmapImage i)
+        {
+            Id = id;
+            Title = t;
+            price = p;
+            image = i;
+        }
+    }
+}
---
+++ Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    internal class Client
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+
+        public List<Book> books { get; set; }
+        public List<Book> newSelectedBooks { get; set; }
+
+
+    }
+}
