e98f490e020edaace43aaf46d4dff3cb77ca770b
SHANAZAR NIZAMADDINOV <snizamaddinow@gmail.com>
2022-05-22 18:14:51 +0300

    Added password check for register. It must consist of only digits.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -115,43 +115,54 @@ namespace BookStore
         } 
         private void register_Click(object sender, RoutedEventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            if ( password.Password != null && 
+                 password.Password.ToString().All(char.IsDigit)) // check if password consists of only digits
             {
-                try
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    connection.Open();
-                    SqlCommand sqlCommand = new SqlCommand(String.Format("Select * from Client where nameSurname='{0}'", username.Text), connection);
-                    
-                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader()) {
-                        if ( sqlDataReader.HasRows && sqlDataReader.Read())
-                        {
-                            MessageBox.Show("User with provided username already exists.\nPlease try different username.", "Invalid username.", MessageBoxButton.OK, MessageBoxImage.Error);
-                        }
-                        else
-                        {
-                            sqlDataReader.Close();
-                            int newUserId = insertNewUser(username.Text, password.Password.ToString(), connection);
-                            if( newUserId != -1)
-                            {
-                                Client client = new Client(newUserId, username.Text, password.Password.ToString());
-                                Store store = new Store(client);
-                                store.Show();
-                                this.Close();
+                    try
+                    {
+                        connection.Open();
+                        SqlCommand sqlCommand = new SqlCommand(String.Format("Select * from Client where nameSurname='{0}'", username.Text), connection);
 
+                        using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader()) {
+                            if (sqlDataReader.HasRows && sqlDataReader.Read())
+                            {
+                                MessageBox.Show("User with provided username already exists.\nPlease try different username.", "Invalid username.", MessageBoxButton.OK, MessageBoxImage.Error);
                             }
                             else
                             {
-                                MessageBox.Show("Could not register.\nPlease try again later!", "Internal error", MessageBoxButton.OK, MessageBoxImage.Error); 
-                            }
-                        }   
-                    }
+                                sqlDataReader.Close();
+                                int newUserId = insertNewUser(username.Text, password.Password.ToString(), connection);
+                                if (newUserId != -1)
+                                {
+                                    Client client = new Client(newUserId, username.Text, password.Password.ToString());
+                                    Store store = new Store(client);
+                                    store.Show();
+                                    this.Close();
 
-                }catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);    
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Could not register.\nPlease try again later!", "Internal error", MessageBoxButton.OK, MessageBoxImage.Error);
+                                }
+                            }
+                        }
+
+                    } catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
                 }
             }
+            else
+            {
+                MessageBox.Show("Password must consist of digits only.\nPlease try again with correct format.", "Invalid password.", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            password.Clear();
         }
+
+        
 
     }
 }
